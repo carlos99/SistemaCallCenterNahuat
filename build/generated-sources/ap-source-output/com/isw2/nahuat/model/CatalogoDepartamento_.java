eb4790805546b65d7bcf8fe087e58b06c9c5cf05
carlos99 <carlos_cw2106@gmail.com>
2013-11-03 00:41:16 -0600

    Eliminando archivos de prueba que estaban en la carpeta 'principales'. Se ha incluido la página index.xhtml y también cambiado el archivo 'MyBundle' y pasado los mensajes de ingles a español. Se ha logrado en la página 'creación-empresa' renderizar correctamente los templates creados. Mencionando lo anterior, se crearon los archivos 'footer', 'header' y 'contents' que van dentro de la carpeta 'templates'. Se han incluido estilos personalizados que van dentro de 'personal-styles'.

--- build/generated-sources/ap-source-output/com/isw2/nahuat/model/CatalogoDepartamento_.java
+++ build/generated-sources/ap-source-output/com/isw2/nahuat/model/CatalogoDepartamento_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.0.v20130507-rNA", date="2013-11-02T11:23:33")
+@Generated(value="EclipseLink-2.5.0.v20130507-rNA", date="2013-11-03T00:34:02")
 @StaticMetamodel(CatalogoDepartamento.class)
 public class CatalogoDepartamento_ { 
 
