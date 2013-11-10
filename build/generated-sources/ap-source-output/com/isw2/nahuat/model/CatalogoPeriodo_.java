1e2e1ad1fd64b05986d42b5d37bbf732b7c10d6b
carlos99 <carlos_cw2106@gmail.com>
2013-11-10 08:49:08 -0600

    Eliminando entidades de paquete model

--- build/generated-sources/ap-source-output/com/isw2/nahuat/model/CatalogoPeriodo_.java
+++ build/generated-sources/ap-source-output/com/isw2/nahuat/model/CatalogoPeriodo_.java
@@ -13,7 +13,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.0.v20130507-rNA", date="2013-11-05T01:47:55")
+@Generated(value="EclipseLink-2.5.0.v20130507-rNA", date="2013-11-09T21:51:13")
 @StaticMetamodel(CatalogoPeriodo.class)
 public class CatalogoPeriodo_ { 
 
--- build/generated-sources/ap-source-output/com/isw2/nahuat/model/EmpleadoReferencia_.java
+++ build/generated-sources/ap-source-output/com/isw2/nahuat/model/EmpleadoReferencia_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.0.v20130507-rNA", date="2013-11-05T01:47:55")
+@Generated(value="EclipseLink-2.5.0.v20130507-rNA", date="2013-11-09T21:51:13")
 @StaticMetamodel(EmpleadoReferencia.class)
 public class EmpleadoReferencia_ { 
 
